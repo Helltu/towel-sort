a6c051b8c3fcbe3a46188bd9442461be99717d72
Hellty <korsovm@gmail.com>
2023-08-06 17:07:11 +0300

    feat: task towel-sort done

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let res = '';
+  if(matrix==null) return res.split('');
+  if(matrix.length==0) return matrix;
+  for(let i=0; i< matrix.length; i++){
+    if((i+1)%2==1) res+=matrix[i]+',';
+    else res+=matrix[i].reverse() +',';
+  }
+  return res.slice(0,-1).split(',');
 }
